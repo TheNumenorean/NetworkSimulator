05c937ec8bd8aa9adb7f8abbddc6327f6d07f87e
Francesco Macagno <fjmacagno1@yahoo.com>
2016-10-27 20:53:43 -0700

    Added input watcher

--- src/main/java/edu/caltech/networksimulator/NetworkSimulator.java
+++ src/main/java/edu/caltech/networksimulator/NetworkSimulator.java
@@ -4,6 +4,7 @@
 package edu.caltech.networksimulator;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * @author Francesco
@@ -12,8 +13,10 @@ import java.util.ArrayList;
 public class NetworkSimulator implements Runnable {
 
 	private ArrayList<NetworkComponent> networkComponents;
-	
+
 	private boolean forceStop;
+
+	private InputListener inputListener;
 
 	/**
 	 * 
@@ -21,6 +24,8 @@ public class NetworkSimulator implements Runnable {
 	public NetworkSimulator() {
 		networkComponents = new ArrayList<NetworkComponent>();
 		forceStop = false;
+		
+		inputListener = new InputListener();
 	}
 
 	/**
@@ -31,7 +36,7 @@ public class NetworkSimulator implements Runnable {
 		NetworkSimulator sim = new NetworkSimulator();
 
 		Link l = new Link("Link1", 10000000, 10, 64000);
-		
+
 		sim.addComponent(l);
 		Host source = new Host("Host1", l, 1000);
 		source.setIP(1);
@@ -40,7 +45,7 @@ public class NetworkSimulator implements Runnable {
 		p.setPayload("Give me all your chocolate or else");
 		source.addPacket(p);
 		sim.addComponent(source);
-		
+
 		Host sink = new Host("Host2", l, 2000);
 		sink.setIP(2);
 		sim.addComponent(sink);
@@ -51,7 +56,9 @@ public class NetworkSimulator implements Runnable {
 
 	/**
 	 * Add a network component to this simulation
-	 * @param comp The component to add
+	 * 
+	 * @param comp
+	 *            The component to add
 	 */
 	public void addComponent(NetworkComponent comp) {
 		networkComponents.add(comp);
@@ -63,6 +70,8 @@ public class NetworkSimulator implements Runnable {
 		for (NetworkComponent n : networkComponents) {
 			new Thread(n).start();
 		}
+		
+		new Thread(inputListener).start();
 
 		while (!forceStop) {
 			try {
@@ -70,19 +79,61 @@ public class NetworkSimulator implements Runnable {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			
 
 			boolean isFinished = true;
 			for (NetworkComponent n : networkComponents) {
 				if (!n.finished())
 					isFinished = false;
 			}
-			
-			if(isFinished)
+
+			if (isFinished)
 				break;
 		}
 		
-		for(NetworkComponent n : networkComponents)
+		inputListener.stop();
+
+		for (NetworkComponent n : networkComponents)
 			n.stop();
+
+		// Complete calculations, get data and print it, etc.
+	}
+	
+	/** 
+	 * Stops the simulator from running, and safely finishes running
+	 */
+	public void stop() {
+		forceStop = true;
+	}
+	
+	/**
+	 * 
+	 * @author Francesco
+	 *
+	 */
+	private class InputListener implements Runnable {
+		
+		private boolean stop;
+
+		@Override
+		public void run() {
+			Scanner scan = new Scanner(System.in);
+			
+			while(!stop) {
+				String input = scan.nextLine();
+				
+				if(input.equalsIgnoreCase("stop"))
+					NetworkSimulator.this.stop();
+			}
+			
+			scan.close();
+			
+		}
+		
+		public void stop() {
+			stop = true;
+		}
+		
 	}
 
 }
